654f5f6fce8ace97947cfa1fbb1c396127fa8d48
Rafael Barros <rafa.a.barros@hotmail.com>
2021-01-26 18:35:41 -0300

    finish init project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Bem vindo ao seu projeto Vue.js App - Rafael Barros"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'Rafael Aparecido barros',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Verdana, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #cc8400;
+  margin-top: 0px;
+  background-color: blue;
+}
+</style>
